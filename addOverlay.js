31138d78cc0247a5e7e1bf5e2bef04f42321f958
Derek Nguyen <tincan24@gmail.com>
2016-08-30 15:31:59 -0700

    Added scripts to display and hide overlay when add on button is clicked

---
+++ addOverlay.js
@@ -0,0 +1,5 @@
+// change the z-index of the overlay to cover video
+
+var overlay = document.getElementById("videoOverlay");
+
+overlay.style.zIndex = '50';
---
+++ removeOverlay.js
@@ -0,0 +1,5 @@
+// change the z-index of the overlay to show the video
+
+var overlay = document.getElementById("videoOverlay");
+
+overlay.style.zIndex = '0';
